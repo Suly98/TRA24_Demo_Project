ee64eba490fbde92b52a243c685d1dadff000517
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-02 19:53:54 +0400

    added item interface

---
+++ src/main/java/com/tra24/ItemInterface.java
@@ -0,0 +1,8 @@
+package com.tra24;
+
+interface itemInterface {
+    public void setPrice(Float price);
+    public void setId(Integer id);
+    public void setQuantity(Integer quantity);
+    public void setName(String name);
+}
