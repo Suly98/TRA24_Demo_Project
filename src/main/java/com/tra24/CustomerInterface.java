734da9e69670174b3d018ce2a407c131507bb7cb
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-02 19:55:54 +0400

    added Customer interface

---
+++ src/main/java/com/tra24/CustomerInterface.java
@@ -0,0 +1,7 @@
+package com.tra24;
+
+public interface CustomerInterface {
+    public void setId(Integer id);
+    public void setFullName(String fullName);
+    public void setPhoneNo(String phoneNumber);
+}
